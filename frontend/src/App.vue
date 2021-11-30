c264e3d233a80a11c4e191dd1b7940c4cdab0bdf
Petrus Briland <petrus.jonsson@gmail.com>
2021-11-30 22:17:01 +0100

    Add some css christmas gifts

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,46 @@
 <template>
-  <div id="nav" class="p-4 bg-green-700 text-gray-200 flex flex-row items-center justify-center">
-    <router-link to="/" class="text-4xl font-bold">
-      * * Advent of Code 2021 * *<br />
-    </router-link>
-  </div>
-  <router-view />
+  <main>
+    <div class="p-4 bg-green-700 text-gray-200 flex flex-row items-center justify-between">
+      <div class="flex flex-row items-end">
+        <div class="present-wrapper present-purple present-sm">
+          <div class="present-top"></div>
+          <div class="present-bottom">
+            <div class="sticker">Merry<br />X-mas</div>
+          </div>
+        </div>
+        <div class="ml-1 present-wrapper present-yellow present-md">
+          <div class="present-top"></div>
+          <div class="present-bottom">
+            <div class="sticker">Merry<br />X-mas</div>
+          </div>
+        </div>
+      </div>
+      <router-link to="/" class="text-4xl font-bold">
+        * * Advent of Code 2021 * *<br />
+      </router-link>
+      <div class="flex flex-row items-end">
+        <div class="present-wrapper">
+          <div class="present-top"></div>
+          <div class="present-bottom">
+            <div class="sticker">Merry<br />X-mas</div>
+          </div>
+        </div>
+        <div class="ml-1 present-wrapper present-green present-sm">
+          <div class="present-top"></div>
+          <div class="present-bottom">
+            <div class="sticker">Merry<br />X-mas</div>
+          </div>
+        </div>
+        <div class="present-wrapper present-red present-lg z-10" style="margin-left: -.5rem;">
+          <div class="present-top"></div>
+          <div class="present-bottom">
+            <div class="sticker">Merry<br />X-mas</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-view />
+  </main>
 </template>
 
 <style>
