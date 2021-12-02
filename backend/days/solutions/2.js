6be2b87998834a62de0121bf274699d86b579dc5
Petrus Briland <petrus.jonsson@gmail.com>
2021-12-02 15:27:15 +0100

    Add day 2

--- backend/days/solutions/2.js
+++ backend/days/solutions/2.js
@@ -10,18 +10,64 @@ function solve (input) {
 
 function p1(puzzle) {
   const start = new Date();
+  const state = executeProgram1(puzzle, {horizontal: 0, depth: 0});
   const duration = new Date() - start;
-  return {value: puzzle.length, duration};
+  return {value: state.horizontal * state.depth, duration};
 }
 
 function p2(puzzle) {
   const start = new Date();
+  const state = executeProgram2(puzzle, {horizontal: 0, depth: 0, aim: 0});
   const duration = new Date() - start;
-  return {value: puzzle.length, duration};
+  return {value: state.horizontal * state.depth, duration};
+}
+
+function executeProgram1(program, state) {
+  const currentState = Object.assign(state);
+  for (const instruction of program) {
+    switch(instruction.type) {
+      case 'forward':
+        currentState.horizontal +=  instruction.value;
+        break;
+      case 'down':
+        currentState.depth += instruction.value;
+        break;
+      case 'up':
+        currentState.depth -= instruction.value;
+        break;
+      default:
+        break;
+        }
+  }
+  return currentState;
+}
+
+function executeProgram2(program, state) {
+  const currentState = Object.assign(state);
+  for (const instruction of program) {
+    switch(instruction.type) {
+      case 'forward':
+        currentState.horizontal +=  instruction.value;
+        currentState.depth += (currentState.aim * instruction.value);
+        break;
+      case 'down':
+        currentState.aim += instruction.value;
+        break;
+      case 'up':
+        currentState.aim -= instruction.value;
+        break;
+      default:
+        break;
+        }
+  }
+  return currentState;
 }
 
 function parseInput(input) {
-  return input.split('\n');
+  return splitLines(input).map(x => {
+    let splitVals = x.split(' ');
+    return {type: splitVals[0], value: parseInt(splitVals[1], 10)};
+  })
 }
 
 module.exports = {solve}
